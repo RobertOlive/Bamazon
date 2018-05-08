73b5bbef412a34b1a582ea8134f9f1626225cc3d
Robert Olive <32848494+RobertOlive@users.noreply.github.com>
2018-05-07 22:53:47 -0400

    allowed user to buy and actually change the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,27 +1,26 @@
 const inquirer = require("inquirer"),
         mysql = require("mysql");
 
-
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "Bamazon"
-});
+    })
           
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    // console.log("connected as id " + connection.threadId + "\n");
     readTable();
-    connection.end();
+    // connection.end();
 
 });
 
 function readTable () {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(res);
+        console.log(res)
         inquirer.prompt([
             {
                 name: "products",
@@ -35,9 +34,10 @@ function readTable () {
                   }
             }
         ]).then(function(answer) {
-            console.log("You've selected " + res[answer.products].product_name + 
-                        ". The price is " + res[answer.products].price + " dollars.")
-            sellItem(res[answer.products].product_name)
+            console.log("You've selected " + res[answer.products-1].product_name + 
+                        ". The price is " + res[answer.products-1].price + " dollars." +
+                        " There are " + res[answer.products-1].stock_quantity + " of these available.")
+            sellItem(res[answer.products-1])
         })
     })
 }
@@ -56,6 +56,17 @@ function sellItem (item) {
           }
         }
     ]).then(function(answers) {
-        console.log("You have purchased " + answers.quantity + " " + item + ".")
+        if (answers.quantity <= item.stock_quantity) {
+            var newQuant = item.stock_quantity - answers.quantity
+            console.log("You have purchased " + answers.quantity + " " + item.product_name + ".")
+            connection.query("UPDATE products SET stock_quantity = " + newQuant + " WHERE item_id = " + item.item_id,
+                function (err, result) {
+                    if (err) console.log(err);
+                    console.log("There are now " + newQuant + " " + item.product_name + " available.")
+                })
+        }
+        else {
+            console.log("There are not enough of those to buy.")
+        }
     })
 }
